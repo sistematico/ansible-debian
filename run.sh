116c6da2836a3ffa5852bce3fdc6ec722fe5fefc
Lucas Saliés Brum <lucas@archlinux.com.br>
2022-04-15 06:14:10 -0400

    Update automático

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+[ "$#" -lt 2 ] && echo "Número errado de argumentos." && exit
+[ ! -f *-$2.yml ] && echo "O playbook não existe." && exit
+
 source $HOME/.keys/vars
-ansible-playbook -i hosts -l $1 *-$2.yml
+
+ansible-playbook -i $1, *-$2.yml
