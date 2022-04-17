3c00a16b8414848bf4913bdb97f4f0f296a8ba8a
Lucas Saliés Brum <lucas@archlinux.com.br>
2022-04-16 22:33:16 -0400

    Update automático

--- scripts/download.sh
+++ scripts/download.sh
@@ -2,15 +2,11 @@
 
 [ ! $1 ] && url='https://www.youtube.com/watch?v=XXYlFuWEuKI' || url="$1"
 
-cd /opt/liquidsoap/music
-
 yt-dlp -i -c \
     -f bestaudio \
     --extract-audio \
     --audio-format mp3 \
     --embed-thumbnail \
     --add-metadata \
-    -o "%(artist)s - %(title)s.%(ext)s" \
+    -o "/opt/liquidsoap/music/%(artist)s - %(title)s.%(ext)s" \
     "$url"
-
-chown -R liquidsoap:liquidsoap /opt/liquidsoap 2> /dev/null
