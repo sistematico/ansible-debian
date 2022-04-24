778b057a6e3ecc113a22c45aa54c088242d6c798
Lucas Saliés Brum <lucas@archlinux.com.br>
2022-04-24 08:38:06 -0400

    Automatic update

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 [ "$1" == "-h" ] && echo "Uso: $(basename $0) [distro] [playbook] [host]" && exit
-[ "$#" -lt 2 ] && echo "Número errado de argumentos." && exit
+[ "$#" -lt 2 ] && echo "Número errado de argumentos.(use -h para ver as opções)" && exit
 [ ! -f $1/*-$2.yml ] && echo "O playbook não existe." && exit
 
 source $HOME/.keys/vars
