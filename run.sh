ae8150c6535460f13add50cd8fa15431d08acbd3
Lucas Saliés Brum <lucas@archlinux.com.br>
2022-05-29 14:38:18 -0400

    Updates

--- run.sh
+++ run.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
-# https://github.com/paramiko/paramiko/issues/2038#issuecomment-1112740435
-sudo pip install fabric 1> /dev/null
-sudo pip uninstall -y cryptography 1> /dev/null # uninstall 37.0.0
-sudo pip install cryptography==36.0.2 1> /dev/null
+[ -f $HOME/.keys/vars ] && source $HOME/.keys/vars
 
-[ "$1" == "-h" ] && echo "Uso: $(basename $0) [distro] [playbook] [host]" && exit
-[ "$#" -lt 2 ] && echo "Número errado de argumentos.(use -h para ver as opções)" && exit
+if [ "$#" -lt 2 ] || [ "$1" == "-h" ]; then 
+    echo "Uso: $(basename $0) [distro] [playbook] [host]"
+    exit
+fi
+
 [ ! -f $1/*-$2.yml ] && echo "O playbook não existe." && exit
-
-source $HOME/.keys/vars
 
 if [ "$3" == "-v" ]; then
     cd $1
